733a388de31cb764100cfcd2fd2a5ac839e7ee98
pedro sassaki <pedropq2001@gmail.com>
2025-10-03 22:09:38 +0900

    add 0 if number is single digit

--- application.js
+++ application.js
@@ -6,7 +6,10 @@ setInterval(() => {
   let currentTime = new Date();
   console.log(currentTime);
 
-  hrs.innerHTML = currentTime.getHours();
-  min.innerHTML = currentTime.getMinutes();
-  sec.innerHTML = currentTime.getSeconds();
+  hrs.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  sec.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1000);
