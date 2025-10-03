2220f9d9d8a1f1dd29e0333592fa88d631167a20
pedro sassaki <pedropq2001@gmail.com>
2025-10-03 22:13:51 +0900

    added notes for futures references

--- application.js
+++ application.js
@@ -1,15 +1,16 @@
-let hrs = document.getElementById("hrs");
+let hrs = document.getElementById("hrs"); // defined hrs to get the element on the html file by id
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
 setInterval(() => {
-  let currentTime = new Date();
+  // every set amount of time it updates
+  let currentTime = new Date(); // defined currentTime to be the current date and time
   console.log(currentTime);
 
   hrs.innerHTML =
-    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours(); // getting hours from currentTime and adding "0" if the number is a single digit
   min.innerHTML =
-    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes(); // getting minutes
   sec.innerHTML =
-    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
-}, 1000);
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds(); // getting seconds
+}, 1000); // 1000 miliseconds = 1 sec
