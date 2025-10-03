dbe0dc904548feacf884613872ab7a243918a76e
pedro sassaki <pedropq2001@gmail.com>
2025-10-03 22:07:54 +0900

    setinterval for every second

--- application.js
+++ application.js
@@ -2,9 +2,11 @@ let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-let currentTime = new Date();
-console.log(currentTime);
+setInterval(() => {
+  let currentTime = new Date();
+  console.log(currentTime);
 
-hrs.innerHTML = currentTime.getHours();
-min.innerHTML = currentTime.getMinutes();
-sec.innerHTML = currentTime.getSeconds();
+  hrs.innerHTML = currentTime.getHours();
+  min.innerHTML = currentTime.getMinutes();
+  sec.innerHTML = currentTime.getSeconds();
+}, 1000);
