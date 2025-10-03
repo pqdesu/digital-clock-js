5870b948ec3e2c88a30358b353e0be11a5026923
pedro sassaki <pedropq2001@gmail.com>
2025-10-03 21:57:33 +0900

    set variable for current time

--- application.js
+++ application.js
@@ -1,3 +1,6 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("secs");
+
+let currentTime = new Date();
+console.log(currentTime);
